15fe7af7da7772724ec003decd5aed69b6de6c33
Canel420 <4521@holbertonstudents.com>
2022-08-25 18:31:45 -0500

    add port and check

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,11 @@
 # Install HTTP header on ubuntu server.
-include stdlib
-
-exec { "system-update":
-    command => "/usr/bin/apt -y update"
-}
-
-package { "Nginx":
-    ensure => "installed" 
-}
-
-file { 'Nginx config':
-  ensure => present,
-  path   => '/etc/nginx/nginx.conf',
-  mode   => '0777'
-}
-
-file_line { 'add http header':
-  path  => '/etc/nginx/nginx.conf',
-  line  => "\tadd_header X-Served-By ${HOSTNAME};",
-  after => 'http {',
-}
-
-exec { 'Restart web server':
-    command => '/usr/bin/service nginx restart'
+exec { 'HTTP_header_Puppet':
+  command  => 'sudo apt -y update;
+               sudo apt -y install nginx;
+               service nginx start;
+               sudo chmod 777 /etc/nginx;
+               sudo chmod 777 /etc/nginx/nginx.conf;
+               sudo sed -i "/sendfile on;/ a \\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf;
+               sudo service nginx restart',
+  provider => 'shell',
 }
