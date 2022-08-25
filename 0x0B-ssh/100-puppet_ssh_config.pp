d1e3e07ed095fc75727c48526cc4628a2fdff9bf
Canel420 <4521@holbertonstudents.com>
2022-08-25 16:46:38 -0500

    stdlib needed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Configuration file server set up in puppet.
 include stdlib
 
-file_line { 'disable password login':
+file_line { 'no-password login':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
-file_line { 'add path to find the keys':
+file_line { 'add path to keys':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }
