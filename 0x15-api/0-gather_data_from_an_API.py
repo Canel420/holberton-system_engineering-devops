327d9dac4c0bbe5dd211276837c45d00530d1521
Canel420 <4521@holbertonstudents.com>
2022-09-09 13:22:54 -0500

    changed print identation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Script that uses a REST API for searching a given employee ID,
-and returns information about his/her TODO list progress.
+and returns information about his/her TO-DO list progress.
 """
 
 from requests import get
@@ -22,7 +22,7 @@ if __name__ == '__main__':
         if task.get('completed'):
             title = task.get('title')
             done_tasks.append(title)
-    print(f'Employee {name} is done with tasks\
-({len(done_tasks)}/{len(tasks)}):')
+    print(f'Employee {name} is done with tasks' +
+          '({len(done_tasks)}/{len(tasks)}):')
     for title in done_tasks:
         print(f'\t {title}')
