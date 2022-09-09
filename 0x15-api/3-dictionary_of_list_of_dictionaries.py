f20fac1b477867c715599cfa9f4423b44159a4c2
Canel420 <4521@holbertonstudents.com>
2022-09-09 15:37:30 -0500

    all users

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Script that uses a REST API for searching a given employee ID,
+and export data into a json format.
+"""
+
+from json import dump
+from requests import get
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = get(url)
+    users = response.json()
+
+    result = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('usernam')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url += '/todos'
+        response = get(url)
+        tasks = response.json()
+        result[user_id] = []
+        for task in tasks:
+            result[user_id].append({
+                                    "task": task.get('title'),
+                                    "completed": task.get('completed'),
+                                    "username": username
+                                    })
+
+    with open('todo_all_employees.json', 'w') as file:
+        dump(result, file)
